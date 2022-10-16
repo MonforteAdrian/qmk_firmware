48d9dee9dd42aa7a54f6ef29f84406bb23391d44
MonforteAdrian <adryanmonforte94@gmail.com>
2022-10-16 14:22:46 +0200

    small changes

--- keyboards/splitkb/kyria/keymaps/hooligan/keymap.c
+++ keyboards/splitkb/kyria/keymaps/hooligan/keymap.c
@@ -39,10 +39,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [QWERTY] = LAYOUT(
-      XXXXXXX,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                                                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    XXXXXXX,
+      KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                                                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    XXXXXXX,
       XXXXXXX,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                                                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, XXXXXXX,
       XXXXXXX,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   XXXXXXX,   XXXXXXX,                           XXXXXXX, XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
-      XXXXXXX,   KC_LGUI, LT(SYM, KC_TAB), KC_SPC, MT(MOD_LCTL, KC_ESC),                   MT(MOD_LSFT, KC_BSPC),  LT(NAV, KC_ENT), LT(NUM,KC_DEL), LT(FUN, KC_CAPS), XXXXXXX
+      XXXXXXX,   KC_LGUI, MO(SYM), KC_SPC, MT(MOD_LCTL, KC_ESC),                   MT(MOD_LSFT, KC_BSPC),  LT(NAV, KC_ENT), LT(NUM,KC_DEL), LT(FUN, KC_CAPS), XXXXXXX
     ),
 /*
  * Lower Layer: Numpad, Media
